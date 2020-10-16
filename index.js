940f597ebafba80acc8433195510d9f9338f3f58
suzukey <suzukei0208@gmail.com>
2020-10-16 10:46:18 +0900

    🎨 Add linter / formatter

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 // This will check if the node version you are running is the required
 // Node version, if it isn't it will throw the following error to inform you.
 if (Number(process.version.slice(1).split(".")[0]) < 12)
-  throw new Error(
-    "Node 12.0.0 or higher is required. Update Node on your system."
-  )
+  throw new Error("Node 12.0.0 or higher is required. Update Node on your system.")
